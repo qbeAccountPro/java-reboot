5b20629a0fbc0d2654b9f8334ea7bd32c08675b1
Quentin Beraud <qbe.pro@yahoo.com>
2026-07-02 00:25:37 +0200

    create interactive console app

--- src/main/java/fr/quentin/Main.java
+++ src/main/java/fr/quentin/Main.java
@@ -1,17 +1,25 @@
 package fr.quentin;
 
+import java.time.Year;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scannerUserInfo = new Scanner(System.in);
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Hello and welcome!");
+        System.out.println("What is your name :");
+        String userName = scannerUserInfo.nextLine();
+        System.out.println("What is your surname :");
+        String userSurname = scannerUserInfo.nextLine();
+        System.out.println("How old are you :");
+        int userAge = scannerUserInfo.nextInt();
+        int actualYear = Year.now().getValue();
+        int yearOfBirth = actualYear - userAge;
+        System.out.println("Welcome \""+userName +" "+userSurname+"\"");
+        System.out.println("You was born in : \""+yearOfBirth+"\"");
         }
-    }
 }
